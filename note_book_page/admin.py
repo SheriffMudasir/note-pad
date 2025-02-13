9338894ed46f8bcea7e659ca225bca61d64b25a2
SheriffMudasir <sheriffmudasir2021@gmail.com>
2025-02-13 20:37:32 +0100

    Routed an home view that displays nothing yet, improve the models with some logic and finally, register the models in the admin

--- note_book_page/admin.py
+++ note_book_page/admin.py
@@ -1,3 +1,27 @@
+from typing import Any
 from django.contrib import admin
+from django.http.request import HttpRequest
 
-# Register your models here.
+from .models import NoteBookPage, Category, Tag
+
+
+class NoteBookPageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'category', 'status', 'created_at')
+    list_filter = ('tags', 'author', 'status')
+    search_fields = ('author', 'category')
+    ordering = ('-created_at',)
+    
+    def get_readonly_fields(self, request, obj=None,):
+        if not obj:
+            return ('author', 'slug')
+        
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        return super().save_model(request, obj, form, change)
+    
+    
+    
+admin.site.register(NoteBookPage, NoteBookPageAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- note_book_page/models.py
+++ note_book_page/models.py
@@ -36,8 +36,9 @@ class NoteBookPage(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        
+        self.slug = slugify(self.title)
+        
         super().save(*args, **kwargs)
     
     def __str__(self) -> str:
